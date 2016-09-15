2ff00cb426668a5ba6a6f20285f8a36018fb43c9
Ryan Everett <reverett4994@gmail.com>
2016-09-15 04:12:06 -0400

    changed subject of feedback mailer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   	# in ActionMailer accepts.
   	def headers
     	{
-     	 :subject => "My Contact Form",
+     	 :subject => "Monitor-Match-Up.com Feedback!",
       	:to => "pattonsrevolver@gmail.com",
       	:from => %("#{name}" <#{email}>)
     	}
